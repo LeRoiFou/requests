d13a3733c9d74fce9113d066fd6d650c72c02d13
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-01-27 18:23:54 +0100

    Add files via upload

--- indexB013_Streamlit.py
+++ indexB013_Streamlit.py
@@ -11,15 +11,19 @@ Objet : présentation des données sous Streamlit / Dash / Power BI
 Lancement sur streamlit : 
 streamlit run indexB013_Streamlit.py
 
+Pour l'insertion des séparateurs de milliers et la devise € :
+https://discuss.streamlit.io/t/formatting-numbers-with-commas-as-thousand-separators-in-st-aggrids-configure-columns-method/25386
+
 Dashboard :
 https://docs.google.com/drawings/d/1OKiRsCjmbV6p6VJojp3Ivz3Hvs2_cKFvSBSMGZ-Qxq0/edit
 
-Prochain travail : Insérer la table
+Prochain travail : Insérer les graphiques
 
 Date : 24-01-24
 """
 
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
 import pandas as pd
 
 def app():
@@ -30,15 +34,15 @@ def app():
     
     # Titre principal de la page @
     st.markdown(
-        "<h1 style='text-align: center; color: yellow;'>Effectifs et masse salariale par zone d'emploi</h1>",
+        "<h3 style='text-align: center; color: yellow;'>Effectifs et masse salariale par zone d'emploi</h3>",
         unsafe_allow_html=True)
     
     # Configuration de la largeur des colonnes
     col1, col2 = st.columns([4, 8])
     
     # Données à afficher dans la colonne n° 1
-    with col1:
-       
+    with col1:  
+        
         # Récupération du fichier .csv converti en DF polars
         job_df = pd.read_csv('data/emploi.csv', sep=";")
         
@@ -53,8 +57,8 @@ def app():
         
         # Insertion de la DF traitée ci-après dans le 1er menu déroulant
         region_st = st.selectbox(
-            "Région",
-            job_f1['region'].sort_values().unique(),
+            "Région", # Titre rattaché au menu déroulant
+            job_f1['region'].sort_values().unique(), # valeurs à insérer
             )
         
         # DF filtrée selon la région sélectionnée dans le menu déroulant ci-avant
@@ -65,10 +69,53 @@ def app():
         
         # Insertion de la DF traitée ci-après dans le 2ème menu déroulant
         area_st = st.selectbox(
-            "Zone d'emploi",
-            job_f2['zone_d_emploi'].sort_values().unique(),
+            "Zone d'emploi", # Titre rattaché au menu déroulant
+            job_f2['zone_d_emploi'].sort_values().unique(), # valeurs à insérer
             )
         
-if __name__ == '__main__':
-    
-    app()
+        # Traitement opérée sur la DF en fonction des valeurs sélectionnés dans les
+        # 2 menus déroulants
+        my_df = job_f1[(job_f1['region'] == region_st) &
+                        (job_f1['zone_d_emploi'] == area_st)]
+        
+        # Champs à afficher
+        my_df = my_df[['annee', 'trimestre', 'effectifs_salaries_cvs',
+                       'masse_salariale_cvs']]
+        
+        # Instanciation de la sous-librairie GridOptionsBuilder
+        gb = GridOptionsBuilder.from_dataframe(my_df)
+
+        # Configuration des colonnes        
+        gb.configure_column(
+            field="annee", # Nom du champ de la DF
+            header_name="Année", # Nom à afficher sur streamlit
+            )
+        gb.configure_column(
+            field="trimestre", # Nom du champ de la DF
+            header_name="Trimestre", # Nom à afficher sur streamlit
+            )  
+        gb.configure_column(
+            field="effectifs_salaries_cvs", # Nom du champ de la DF
+            header_name="Effectifs", # Nom à afficher sur streamlit 
+            type=["numericColumn","numberColumnFilter","customNumericFormat"], 
+            valueFormatter="data.effectifs_salaries_cvs.toLocaleString('fr-FR')",
+            ) # type et valueFormatter : séparateur de milliers
+        gb.configure_column(
+            field="masse_salariale_cvs", # Nom du champ de la DF
+            header_name="Salaires", # Nom à afficher sur streamlit
+            type=["numericColumn","numberColumnFilter","customNumericFormat"], 
+            valueFormatter="data.masse_salariale_cvs.toLocaleString('fr-FR', {style: 'currency', currency: 'EUR', maximumFractionDigits:0})",
+            ) # type et valueFormatter : séparateur de milliers et devise €
+        
+        # Mise en place des configurations de la table ci-avant
+        go = gb.build()
+        
+        # Affichage de la table
+        AgGrid(my_df, 
+               gridOptions=go, # configuration des colonnes ci-avant
+               fit_columns_on_grid_load=True, # ajustement automatique des colonnes
+               height=350, # hauteur de la table
+               )
+
+# Lancement de l'application        
+app()
